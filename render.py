810ce662137a554361e8958ff8aadbdbe3b6f197
harveynw <harveywilliams@live.com>
2023-03-02 17:11:43 +0000

    Broadcast will take care of this

--- render.py
+++ render.py
@@ -3,6 +3,12 @@ import torch
 T = torch.tensor
 
 
+def _nan_check(x : T, name: str):
+    count = torch.sum(torch.isnan(x)).to('cpu')
+    if count > 0:
+        print(f'ALERT check for NaN in {name} found {count}')
+
+
 def gamma_encoding(p: T, l: int, device: str = 'cpu') -> T:  # Eqn. 4, p is (batch_size, dim)
     coef = torch.pow(2, torch.arange(start=0, end=l, step=1)).to(device) * torch.pi
 
@@ -26,7 +32,7 @@ def expected_colour(N: int, nerf: torch.nn, o: T, d: T, t_n: T, t_f: T, device: 
     t = torch.rand(batch_size, N, device=device) * (upper - lower) + lower
     t = t.unsqueeze(2)
 
-    o_t, d_t = o.unsqueeze(1).repeat(1, N, 1), d.unsqueeze(1).repeat(1, N, 1)
+    o_t, d_t = o.unsqueeze(1), d.unsqueeze(1)  # batch_size x 1 x 3
     positions = o_t + t * d_t
     positions = positions.view(batch_size*N, 3)  # batch_size*N x 3
 
@@ -34,7 +40,12 @@ def expected_colour(N: int, nerf: torch.nn, o: T, d: T, t_n: T, t_f: T, device: 
     position_encoded = gamma_encoding(positions, 10, device=device)
     direction_encoded = gamma_encoding(d, 4, device=device).repeat(N, 1)
 
+    _nan_check(position_encoded, 'Position Encoded'), _nan_check(direction_encoded, 'Direction Encoded')
+
     σ_i, c_i = nerf(position_encoded, direction_encoded)
+
+    _nan_check(σ_i, 'NN Output Volume Density')
+    _nan_check(c_i, 'NN Output Radiance')
 
     σ_i = σ_i.view(batch_size, N)  # batch_size x N
     c_i = c_i.view(batch_size, N, 3)  # batch_size x N x 3
@@ -46,6 +57,8 @@ def expected_colour(N: int, nerf: torch.nn, o: T, d: T, t_n: T, t_f: T, device: 
     T_i_inner = -σ_i*δ_i
     T_i_inner = torch.cat([torch.zeros(batch_size, 1, device=device), T_i_inner[:, :-1]], dim=1)
     T_i = torch.exp(torch.cumsum(T_i_inner, dim=1))
+
+    _nan_check(T_i, 'T_i')
 
     coef_term = 1 - torch.exp(-σ_i*δ_i)
     coef = T_i * coef_term
